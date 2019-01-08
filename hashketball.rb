ac884a3d9d1c730bad66395dee6ccd6b180a6550
Dylan Clements <dclements@protonmail.com>
2019-01-08 18:02:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -172,7 +172,7 @@ end
 
 def big_shoe_rebounds
     largest_shoes = 0
-    rebounds = 0 
+    rebounds = 0
   game_hash.each do |location, team_data|
       team_data[:players].each do |player, data|
           if data[:shoe] > largest_shoes
